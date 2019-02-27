e18c3aa3f1e0bdb9f4d436de6b8b82b89b9c85ab
Shi Guo <shiguo@Shi-Guos-MacBook-Air.local>
2019-02-27 15:58:57 -0600

    change author

--- src/basis_function/SO_4D_CPlanewave_function.h
+++ src/basis_function/SO_4D_CPlanewave_function.h
@@ -1,6 +1,6 @@
 /*
  
-Copyright (C) 2008 Jindrich Kolorenc
+Copyright (C) 2012 Shi Guo
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
